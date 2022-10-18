31de6d8caef1fdd5ffef4dc3165a7b1ff058248b
rimatik <slaven@rimatik.hr>
2022-10-18 16:01:18 +0200

    added validation pipe and create-address api validation

--- src/address/dto/create-address.dto.ts
+++ src/address/dto/create-address.dto.ts
@@ -1,22 +1,42 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  Matches,
+} from 'class-validator';
 import { ObjectId } from 'mongoose';
+import { nearWalletRegex } from 'src/utils/regex';
 
 export class CreateAddressDto {
   @ApiProperty({
     type: String,
     required: true,
   })
+  @IsNotEmpty()
+  @Matches(nearWalletRegex)
   wallet: string;
   @ApiProperty({
     type: String,
     required: true,
   })
+  @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    required: false,
+  })
   alias: string;
+
+  @IsEmail()
+  @IsOptional()
   @ApiProperty({
     type: String,
     required: false,
   })
   email: string;
+  @IsPhoneNumber()
+  @IsOptional()
   @ApiProperty({
     type: String,
     required: false,
