075eaaf761fd5062a4001fb6ace15c4be8a42ad9
rimatik <slaven@rimatik.hr>
2022-10-18 17:51:26 +0200

    added address findAll, update, delete apis

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,17 +1,25 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
   HttpCode,
   HttpException,
+  NotFoundException,
+  Param,
+  Patch,
   Post,
+  Query,
   Req,
+  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { AuthRequest } from 'src/user/entities/user.entity';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
+import { UpdateAddressDto } from './dto/update-address.dto';
 import { Address } from './entities/address.entity';
 
 @ApiTags('address')
@@ -25,12 +33,102 @@ export class AddressController {
   @ApiResponse({ status: 200, type: Address })
   @ApiResponse({ status: 201, description: 'Address created' })
   @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 404, description: 'Not found' })
   @ApiResponse({ status: 500, description: 'Server error' })
   @HttpCode(200)
   async create(@Req() request: AuthRequest, @Body() dto: CreateAddressDto) {
     dto.owner = request.user._id;
     const result = await this.addressService.create(dto);
+    if (result.data) {
+      return result.data;
+    }
+
+    throw new HttpException(result.error.message, result.error.code);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  @ApiBearerAuth()
+  @ApiQuery({ name: 'id', required: false })
+  @ApiResponse({ status: 200, type: [Address] })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 404, description: 'Not found' })
+  @ApiResponse({ status: 500, description: 'Server error' })
+  async findAll(@Query('id') id?: string) {
+    if (id) {
+      const result = await this.addressService.findOne(id);
+      if (result.data) {
+        return result.data;
+      }
+
+      throw new HttpException(result.error.message, result.error.code);
+    }
+
+    const result = await this.addressService.findAll();
+    if (result.data) {
+      return result.data;
+    }
+
+    throw new HttpException(result.error.message, result.error.code);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, type: Address })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 404, description: 'Not found' })
+  @ApiResponse({ status: 500, description: 'Server error' })
+  async update(
+    @Req() request: AuthRequest,
+    @Param('id') id: string,
+    @Body() updateProjectDto: UpdateAddressDto,
+  ) {
+    const address = await this.addressService.findOne(id);
+    if (address.data) {
+      if (address.data.owner.uid !== request.user.uid)
+        throw new UnauthorizedException();
+    } else {
+      throw new HttpException(address.error.message, address.error.code);
+    }
+
+    const result = await this.addressService.update(id, updateProjectDto);
+
+    if (result.data) {
+      return result.data;
+    }
+
+    throw new HttpException(result.error.message, result.error.code);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, type: Address })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 404, description: 'Not found' })
+  @ApiResponse({ status: 500, description: 'Server error' })
+  async remove(@Req() request: AuthRequest, @Param('id') id: string) {
+    const address = await this.addressService.findOne(id);
+    if (address.data) {
+      if (!address.data) {
+        throw new NotFoundException('Address not found!');
+      }
+
+      if (address.data.owner.uid !== request.user.uid)
+        throw new UnauthorizedException();
+    }
+
+    const result = await this.addressService.remove(id);
+
     if (result.data) {
       return result.data;
     }
--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import Mongose, { Model } from 'mongoose';
 import { ServiceError } from 'src/common/models/ServiceError';
 import { ServiceResult } from 'src/common/models/ServiceResult';
 import { isNearWallet } from 'src/utils/nearWalletValidation';
 import { CreateAddressDto } from './dto/create-address.dto';
+import { UpdateAddressDto } from './dto/update-address.dto';
 import { Address, AddressDocument } from './entities/address.entity';
 
 @Injectable()
@@ -51,6 +52,85 @@ export class AddressService {
       return new ServiceResult<Address>(result);
     } catch (error) {
       this.logger.error('AddressService - create', error);
+      return new ServiceResult<Address>(
+        null,
+        new ServiceError(500, `Server error`),
+      );
+    }
+  }
+
+  async findAll(): Promise<ServiceResult<Address[]>> {
+    try {
+      const addresses = await this.repo.find().populate('owner').exec();
+      return new ServiceResult<Address[]>(addresses);
+    } catch (error) {
+      this.logger.error('AddressService - findAll', error);
+      return new ServiceResult<Address[]>(
+        null,
+        new ServiceError(500, `Server error`),
+      );
+    }
+  }
+
+  async update(
+    id: string,
+    updateAddressDto: UpdateAddressDto,
+  ): Promise<ServiceResult<Address>> {
+    try {
+      if (!Mongose.Types.ObjectId.isValid(id)) {
+        return new ServiceResult<Address>(
+          null,
+          new ServiceError(404, `Not found`),
+        );
+      }
+      const address = await this.repo
+        .findByIdAndUpdate(id, updateAddressDto)
+        .exec();
+      return new ServiceResult<Address>(address);
+    } catch (error) {
+      this.logger.error('AddressService - update', error);
+      return new ServiceResult<Address>(
+        null,
+        new ServiceError(500, `Server error`),
+      );
+    }
+  }
+
+  async findOne(id: string): Promise<ServiceResult<Address>> {
+    try {
+      if (!Mongose.Types.ObjectId.isValid(id)) {
+        return new ServiceResult<Address>(
+          null,
+          new ServiceError(404, `Not found`),
+        );
+      }
+      const address = await this.repo
+        .findOne({ _id: id })
+        .populate('owner')
+        .exec();
+      return new ServiceResult<Address>(address);
+    } catch (error) {
+      this.logger.error('AddressService - findOne', error);
+      return new ServiceResult<Address>(
+        null,
+        new ServiceError(500, `Server error`),
+      );
+    }
+  }
+
+  async remove(id: string): Promise<ServiceResult<Address>> {
+    try {
+      if (!Mongose.Types.ObjectId.isValid(id)) {
+        return new ServiceResult<Address>(
+          null,
+          new ServiceError(404, `Not found`),
+        );
+      }
+
+      const result = await this.repo.findByIdAndDelete(id).exec();
+      return new ServiceResult<Address>(result);
+    } catch (error) {
+      this.logger.error('AddressService - findOne', error);
       return new ServiceResult<Address>(
         null,
         new ServiceError(500, `Server error`),
