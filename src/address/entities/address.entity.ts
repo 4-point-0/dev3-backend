b158c00c108965829cc1ee1cac6e6a9e7cb27db9
rimatik <slaven@rimatik.hr>
2022-10-18 16:03:05 +0200

    added swagger ApiProperty annotations to address.entity.ts

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import mongoose, { Document, ObjectId } from 'mongoose';
 import { User } from 'src/user/entities/user.entity';
 
@@ -10,24 +11,45 @@ export type AddressDocument = Address & Document;
 export class Address {
   _id: ObjectId;
 
+  @ApiProperty({
+    type: Date,
+  })
   @Prop({ type: Date, default: Date.now })
   updatedAt: Date;
 
+  @ApiProperty({
+    type: Date,
+  })
   @Prop({ type: Date, default: Date.now })
   createdAt: Date;
 
+  @ApiProperty({
+    type: String,
+  })
   @Prop({ required: true })
   wallet: string;
 
+  @ApiProperty({
+    type: String,
+  })
   @Prop({ required: true })
   alias: string;
 
+  @ApiProperty({
+    type: String,
+  })
   @Prop({ required: false })
   phone: string;
 
+  @ApiProperty({
+    type: String,
+  })
   @Prop({ required: false })
   email: string;
 
+  @ApiProperty({
+    type: User,
+  })
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: User.name })
   owner: User;
 }
