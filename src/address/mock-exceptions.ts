1ac5e28ae2994fb57079cc76ab249fad0dc6a36e
rimatik <slaven@rimatik.hr>
2022-10-24 16:56:50 +0200

    removed mock-exceptions

--- src/address/mock-exceptions.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class AdressNotUnique extends HttpException {
-  constructor() {
-    super("Wallet isn't unique!", HttpStatus.BAD_REQUEST);
-  }
-}
