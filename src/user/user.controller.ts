a070081d7c3ed8616bb4467e89a5645205c2d781
rimatik <slaven@rimatik.hr>
2022-10-27 17:51:15 +0200

    commented out user controller methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,35 +1,35 @@
 import {
-  Body,
+  // Body,
   Controller,
-  Delete,
+  // Delete,
   Get,
-  Param,
-  Patch,
-  Post,
+  // Param,
+  // Patch,
+  // Post,
   Req,
   UseGuards,
 } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+// import { JwtService } from '@nestjs/jwt';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { Role } from '../auth/role.enum';
-import { Roles } from '../auth/roles.decorators';
-import { RolesGuard } from '../auth/roles.guard';
-import { Project } from '../project/entities/project.entity';
-import { ProjectService } from '../project/project.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+// import { Role } from '../auth/role.enum';
+// import { Roles } from '../auth/roles.decorators';
+// import { RolesGuard } from '../auth/roles.guard';
+// import { Project } from '../project/entities/project.entity';
+// import { ProjectService } from '../project/project.service';
+// import { CreateUserDto } from './dto/create-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthRequest, User } from './entities/user.entity';
-import { UserService } from './user.service';
+// import { UserService } from './user.service';
 
 @ApiTags('user')
 @ApiBearerAuth()
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly userService: UserService,
-    private readonly projectService: ProjectService,
-  ) {}
+  // constructor()
+  // private readonly userService: UserService,
+  // private readonly projectService: ProjectService,
+  // {}
 
   @UseGuards(JwtAuthGuard)
   @Get('me')
@@ -38,12 +38,12 @@ export class UserController {
     return request.user;
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('projects/owned')
-  @ApiResponse({ status: 200, type: [Project] })
-  projectsOwned(@Req() request: AuthRequest) {
-    return this.projectService.findAllForOwner(request.user._id);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Get('projects/owned')
+  // @ApiResponse({ status: 200, type: [Project] })
+  // projectsOwned(@Req() request: AuthRequest) {
+  //   return this.projectService.findAll(request.user._id);
+  // }
 
   // @Roles(Role.Admin)
   // @UseGuards(JwtAuthGuard, RolesGuard)
@@ -53,39 +53,39 @@ export class UserController {
   //   return this.projectService.findAll(true);
   // }
 
-  @Roles(Role.Admin)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
+  // @Roles(Role.Admin)
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.userService.create(createUserDto);
+  // }
 
-  @Roles(Role.Admin)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Get()
-  @ApiResponse({ status: 200, type: [User] })
-  findAll() {
-    return this.userService.findAll();
-  }
+  // @Roles(Role.Admin)
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Get()
+  // @ApiResponse({ status: 200, type: [User] })
+  // findAll() {
+  //   return this.userService.findAll();
+  // }
 
-  @Roles(Role.Admin)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Get(':uid')
-  findOne(@Param('uid') id: string) {
-    return this.userService.findOne(id);
-  }
+  // @Roles(Role.Admin)
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Get(':uid')
+  // findOne(@Param('uid') id: string) {
+  //   return this.userService.findOne(id);
+  // }
 
-  @Roles(Role.Admin)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto);
-  }
+  // @Roles(Role.Admin)
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(id, updateUserDto);
+  // }
 
-  @Roles(Role.Admin)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
+  // @Roles(Role.Admin)
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(+id);
+  // }
 }
