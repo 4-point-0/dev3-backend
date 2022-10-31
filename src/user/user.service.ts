5713948cc4364623cb82b488c41f7b8a3dad87cf
rimatik <slaven@rimatik.hr>
2022-10-31 14:58:34 +0100

    removed unused user.service methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument } from './entities/user.entity';
 
 @Injectable()
@@ -14,25 +13,9 @@ export class UserService {
     return created.save();
   }
 
-  findAll(): Promise<User[]> {
-    return this.repo.find().exec();
-  }
-
   findOne(uid: string): Promise<User> {
     return this.repo
       .findOne({ $or: [{ uid }, { nearWalletAccountId: uid }] })
       .exec();
-  }
-
-  findOneById(id: string): Promise<User> {
-    return this.repo.findOne({ _id: id }).exec();
-  }
-
-  update(id: string, updateUserDto: UpdateUserDto) {
-    return this.repo.findByIdAndUpdate(id, updateUserDto).exec();
-  }
-
-  remove(id: number) {
-    return this.repo.findByIdAndDelete(id).exec();
   }
 }
