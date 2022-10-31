89d8d18f59fed43b22167e805ae1614397a7ef0b
rimatik <slaven@rimatik.hr>
2022-10-31 11:08:42 +0100

    added staging clause to getRpc

--- src/helpers/rpc/rpc-helper.ts
+++ src/helpers/rpc/rpc-helper.ts
@@ -5,7 +5,7 @@ import { rpcMainnet, rpcTestnet } from './rpc-constants';
 const { NODE_ENV } = process.env;
 
 export const getRpc = (): string => {
-  if (NODE_ENV === 'development') {
+  if (NODE_ENV === 'dev' || NODE_ENV === 'staging') {
     return rpcTestnet;
   }
 
