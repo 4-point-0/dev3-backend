9c9e624805f8b1cb7e83f2ce66a697d44c17a658
rimatik <slaven@rimatik.hr>
2022-11-03 10:04:40 +0100

    added PAGODA_BEARER to jwtConstants

--- src/modules/auth/common/jwt-constants.ts
+++ src/modules/auth/common/jwt-constants.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, PAGODA_BEARER } = process.env;
 
 export const jwtConstants = {
   secret: JWT_SECRET,
+  pagodaBearer: PAGODA_BEARER,
 };
