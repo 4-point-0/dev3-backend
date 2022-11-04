80c58107c945edde894712d8f082bc9219be7855
rimatik <slaven@rimatik.hr>
2022-11-04 10:37:28 +0100

    removed unused dtos

--- src/modules/payment/dto/pagoda-payload.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PagodaEventDto } from './pagoda-event.dto';
-
-export class PagodaPayloadDto {
-  Events: PagodaEventDto;
-}
--- src/modules/payment/dto/pagoda.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PagodaPayloadDto } from './pagoda-payload.dto';
-
-export class PagodaDto {
-  chain_id: string;
-  alert_rule_id: number;
-  alert_name: string;
-  payload: PagodaPayloadDto;
-}
