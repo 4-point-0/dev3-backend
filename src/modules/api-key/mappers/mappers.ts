6d388f4b8669a7b92bd74ccf7b58386e5a09c00e
rimatik <slaven@rimatik.hr>
2022-11-16 11:57:43 +0100

    added api key module with create,findAll, findOne,isValid,regenerate,revoke apis

---
+++ src/modules/api-key/mappers/mappers.ts
@@ -0,0 +1,26 @@
+import { PaginatedDto } from '../../../common/pagination/paginated-dto';
+import { ApiKeyDto } from '../dto/api-key.dto';
+import { ApiKey } from '../entities/api-key.entity';
+
+export const mapToApiKeyDto = (apiKey: any): ApiKeyDto => {
+  return {
+    id: apiKey.id,
+    created_at: apiKey.createdAt,
+    expires: apiKey.expires,
+    is_revoked: apiKey.is_revoked,
+    api_key: apiKey.api_key,
+    project_id: apiKey.project._id,
+  };
+};
+
+export const mapToPaginatedApiKeyDto = (
+  dto: PaginatedDto<ApiKey>,
+  results: ApiKeyDto[],
+): PaginatedDto<ApiKeyDto> => {
+  return {
+    total: dto.total,
+    limit: dto.limit,
+    offset: dto.offset,
+    results: results,
+  };
+};
